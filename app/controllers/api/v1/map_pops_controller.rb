48083244ebf7fb3181b2669445f148ccae8e31c0
Diego Costa <105527332+Dieg0CLima@users.noreply.github.com>
2026-02-23 16:13:10 -0300

    Fix map pop create payload id/external_id consistency

--- app/controllers/api/v1/map_pops_controller.rb
+++ app/controllers/api/v1/map_pops_controller.rb
@@ -6,18 +6,19 @@ class Api::V1::MapPopsController < ApplicationController
   before_action :require_editor_or_admin!, only: %i[create update destroy]
 
   def index
-    render json: { data: @network_map.map_pops.order(:id) }, status: :ok
+    pops = @network_map.map_pops.order(:id)
+    render json: { data: pops.map { |pop| map_pop_payload(pop) } }, status: :ok
   end
 
   def show
-    render json: { data: @map_pop }, status: :ok
+    render json: { data: map_pop_payload(@map_pop) }, status: :ok
   end
 
   def create
     map_pop = @network_map.map_pops.new(map_pop_params)
 
     if map_pop.save
-      render json: { data: map_pop }, status: :created
+      render json: { data: map_pop_payload(map_pop) }, status: :created
     else
       render json: { errors: map_pop.errors.full_messages }, status: :unprocessable_entity
     end
@@ -25,7 +26,7 @@ class Api::V1::MapPopsController < ApplicationController
 
   def update
     if @map_pop.update(map_pop_params)
-      render json: { data: @map_pop }, status: :ok
+      render json: { data: map_pop_payload(@map_pop) }, status: :ok
     else
       render json: { errors: @map_pop.errors.full_messages }, status: :unprocessable_entity
     end
@@ -49,10 +50,29 @@ class Api::V1::MapPopsController < ApplicationController
   end
 
   def set_map_pop
-    @map_pop = @network_map.map_pops.find(params[:id])
+    @map_pop = @network_map.map_pops.find_by(external_id: params[:id])
+    return if @map_pop.present?
+
+    @map_pop = @network_map.map_pops.find(params[:id]) if params[:id].to_s.match?(/\A\d+\z/)
+    raise ActiveRecord::RecordNotFound if @map_pop.blank?
   end
 
   def map_pop_params
     params.require(:map_pop).permit(:name, :external_id, :lat, :lng, :color, metadata: {})
+  end
+
+  def map_pop_payload(pop)
+    {
+      id: pop.external_id || pop.id,
+      network_map_id: pop.network_map_id,
+      name: pop.name,
+      external_id: pop.external_id,
+      lat: pop.lat,
+      lng: pop.lng,
+      color: pop.color,
+      metadata: pop.metadata,
+      created_at: pop.created_at,
+      updated_at: pop.updated_at
+    }
   end
 end
